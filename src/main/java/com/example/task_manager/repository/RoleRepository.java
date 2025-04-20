446626a0f70b83d8800a627f2e940098cd2fae2d
Ines <benkhaledines510@gmail.com>
2025-04-20 11:44:27 +0100

    Added repositories for User, Role, and Task

---
+++ src/main/java/com/example/task_manager/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.task_manager.repository;
+
+import com.example.task_manager.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
